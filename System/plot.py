206ac92ff8d34519369be4475b5c38b3dcc6f86e
Tom Bensky <tbensky@gmail.com>
2024-07-16 14:58:50 -0700

    updates

--- System/plot.py
+++ System/plot.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 
 df = pd.read_csv("trajectory.csv")
-plt.plot(df['x'],df['y'])
+plt.plot(df['x'],df['y'],'.')
 plt.xlabel("x (m)")
 plt.ylabel("y (m)")
 plt.title("Projectile trajectory with drag")
